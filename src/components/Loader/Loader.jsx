989d48938ba7b5255cdf9c4e80dd513b7c6e9bdd
OJailson17 <JaylsonO17@gmail.com>
2021-07-26 01:28:13 -0300

    Loader component created

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+import DotLoader from "react-spinners/DotLoader";
+
+const LoaderContainer = styled.div`
+width: 100%;
+height: 500px;
+/* background: red; */
+display: flex;
+justify-content: center;
+align-items: center;
+`
+
+export function Loader({isReady}) {
+    return (
+        <LoaderContainer>
+        <DotLoader size={100} color="#0288d1" loading={isReady} />
+        </LoaderContainer>
+    )
+}

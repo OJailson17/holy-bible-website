34f753dd631fa65a48a1718fd32580caaa2c21bb
OJailson17 <JaylsonO17@gmail.com>
2022-12-10 22:09:35 -0300

    refact favorit list check function

--- src/components/helper/checkFavoriteList.js
+++ src/components/helper/checkFavoriteList.js
@@ -1,14 +1,10 @@
-/* eslint-disable eqeqeq */
-export const checkFavoriteList = (verse) => {
-    const favoriteList = JSON.parse(localStorage.getItem("favorites")) || []
+import _ from 'lodash';
 
-    const checkName = favoriteList.filter(el => el.name === verse.name)
-    const checkChapter = favoriteList.filter(el => el.chapter === verse.chapter)
-    const checkVerse = favoriteList.filter(el => el.verse === verse.verse)
-    
-    if(checkName.length > 0 && checkChapter.length > 0 && checkVerse.length > 0) {
-      return true
-    } else {
-      return false
-    }
-}
+/* eslint-disable eqeqeq */
+export const checkFavoriteList = verse => {
+	const favoriteList = JSON.parse(localStorage.getItem('favorites')) || [];
+
+	const verseExists = favoriteList.filter(el => _.isMatch(el, verse) ?? el);
+
+	return verseExists.length > 0 ? true : false;
+};
